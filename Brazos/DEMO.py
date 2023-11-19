3a78259a8ff31fcc0c65a2863d10960f63fb590d
Oliver Rojas <oliver.rp2003@gmail.com>
2023-11-19 01:29:39 -0500

    Demo creation + metodos para  codigo limpio

---
+++ Brazos/DEMO.py
@@ -0,0 +1,90 @@
+# Proyecto de SIGA
+# Grupo 3
+
+##Desarrollado por:
+## Oliver Rojas Pumaricra
+
+## El siguiente codigo muestra la determinación de los angulos en tiempo real
+
+
+
+import cv2
+# import mediapipe as mp
+import time
+import PoseModule as pm
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# import csv
+
+cap = cv2.VideoCapture('PoseVideos/curls.mp4')  # Poner el nombre de la carpeta del video que usarás
+# cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)
+pTime = 0
+detector = pm.poseDetector()
+count = 0
+
+cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)
+while True:
+    success, img = cap.read()
+
+    # IF: Como filtro de valores atípicos
+    if img is not None:
+        img = detector.findPose(img, False)
+        lmList = detector.findPosition(img, False)
+
+        #SI se encontró un cuerpo se procesará y mostrará esa data
+        if len(lmList) != 0:
+            #CALCULO BRAZO--------------------------------------------------------
+            # Angulo de brazo respecto al torso (puntuaciones)
+
+            # DERECHA ---------------------------
+            BRder = abs(detector.findAngle3D(img, 14, 12, 24))
+            PBRder= detector.brazoPuntuation(BRder)
+
+            # IZQUIERDA ---------------------------
+            BRizq = abs(detector.findAngle3D(img, 13, 11, 23))
+            PBRizq = detector.brazoPuntuation(BRizq)
+
+            # CALCULO ANTEBRAZOS ---------------------------------------------------------------
+            # DERECHA ---------------------------
+            ABRder = abs(detector.findAngle3D(img, 12, 14, 16))
+            PABRder = detector.anteBrazoPuntuation(BRder)
+
+            # IZQUIERDA ---------------------------
+            ABRizq = abs(detector.findAngle3D(img, 11, 13, 15))
+            PABRizq = detector.anteBrazoPuntuation(BRizq)
+
+            # CALCULO CUELLO ---------------------------------------------------------------
+            #Promediacion
+            head=[]
+            budy=[]
+            head = detector.promDot(img,7,8)
+            budy = detector.promDot(img, 23, 24)
+            cuello= detector.promDot(img,11,12)
+
+            ANGcuello = (detector.findAngle3DbyVectors(img, head, cuello, budy, draw=True))
+            PCuello = detector.cuelloPuntuation
+
+            # PUNTAJE TOTAL
+            TOTAL= PBRder + PBRizq + ABRder + PABRizq + PCuello #Todos estos valores impresos en el forms
+
+
+
+        # Reajuste de tamaños: si la imagen sale muy grande o pequeña solo cambia el valor de la escala
+        escala = 1
+        width, height, _ = img.shape
+        alto = int(width * escala)
+        ancho = int(height * escala)
+        img = cv2.resize(img, (ancho, alto))
+        cv2.imshow("Image", img)
+        cv2.waitKey(1)
+
+        count += 1
+
+    else:
+        ##En caso haya un error, se mantiene el valor anterior
+        pass
+
+
+
--- Brazos/PoseModule.py
+++ Brazos/PoseModule.py
@@ -16,7 +16,10 @@ class poseDetector():
         self.mpDraw = mp.solutions.drawing_utils
         self.mpPose = mp.solutions.pose
         self.pose = self.mpPose.Pose(self.mode, self.upBody, self.smooth)
+        #self.pose.setUseGpu(True)
         #self.pose = self.mpPose.Pose(self.mode, self.upBody, self.smooth, self.detectionCon, self.trackCon)
+
+
 
     def findPose(self, img, draw=True):
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -78,7 +81,117 @@ class poseDetector():
 
             #cv2.putText(img, str(int(distancia)), ((x1+x2)*0.5-56,(y1+y2)*0.5+58), cv2.FONT_HERSHEY_PLAIN,2,(0,0,255),2)
         return distancia
-    
+
+    def findAngle3D(self, img, p1, p2, p3, draw = True):
+        z1, x1, y1 = self.lmList[p1]
+        z2, x2, y2 = self.lmList[p2]
+        z3, x3, y3 = self.lmList[p3]
+        #print(x1," , ",y1, " , ",z1)
+        # Calculo de angulo
+        vector_a = np.array([x2-x1, y2-y1, z2-z1])
+        vector_b = np.array([x2-x3, y2-y3, z2-z3])
+
+        # Calcular el producto punto (producto escalar) de los vectores
+        producto_punto = np.dot(vector_a, vector_b)
+
+        # Modulo de cada vector
+        magnitud_a = np.linalg.norm(vector_a)
+        magnitud_b = np.linalg.norm(vector_b)
+
+        angulo_radianes = np.arccos(producto_punto / (magnitud_a * magnitud_b))
+        angulo_grados = np.degrees(angulo_radianes)
+
+        #Logica para solo obtusos maximo
+        #if angulo_grados>180
+        # Draw
+        if draw:
+            cv2.line(img, (x1, y1), (x2, y2), (0, 255, 0, 3), 2)
+            cv2.line(img, (x3, y3), (x2, y2), (0, 255, 0, 3), 2)
+
+            cv2.circle(img, (x1, y1), 7, (255, 0, 0), cv2.FILLED)
+            cv2.circle(img, (x1, y1), 10, (255, 0, 0), 2)
+
+            cv2.circle(img, (x2, y2), 7, (255, 0, 0), cv2.FILLED)
+            cv2.circle(img, (x2, y2), 10, (255, 0, 0), 2)
+
+            cv2.circle(img, (x3, y3), 7, (255, 0, 0), cv2.FILLED)
+            cv2.circle(img, (x3, y3), 10, (255, 0, 0), 2)
+
+            cv2.putText(img, str(int(angulo_grados)), (x2 - 56, y2 + 58), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 2)
+        return angulo_grados
+
+    def findAngle3DbyVectors(self, img, p1,p2,p3, draw = True):
+        z1, x1, y1 = p1
+        z2, x2, y2 = p2
+        z3, x3, y3 = p3
+
+        vector_a = np.array([x2 - x1, y2 - y1, z2 - z1])
+        vector_b = np.array([x2 - x3, y2 - y3, z2 - z3])
+        # Calcular el producto punto (producto escalar) de los vectores
+        producto_punto = np.dot(vector_a, vector_b)
+
+        # Modulo de cada vector
+        magnitud_a = np.linalg.norm(vector_a)
+        magnitud_b = np.linalg.norm(vector_b)
+
+        angulo_radianes = np.arccos(producto_punto / (magnitud_a * magnitud_b))
+        angulo_grados = np.degrees(angulo_radianes)
+
+        producto_cruzado = np.cross(vector_a, vector_b)
+        if producto_cruzado[2] < 0:  # Verificar el signo del producto cruzado para ajustar el ángulo
+            angulo_grados = 360 - angulo_grados
+
+            #Logica para solo obtusos maximo
+        #if angulo_grados>180
+        # Draw
+        if draw:
+            cv2.putText(img, str(int(angulo_grados)), (int(x2) - 56, int(y2) + 58), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 2)
+        return angulo_grados
+
+    def anteBrazoPuntuation(self, ang):
+        # p para tener un codigo más limpio
+        if ang > 60 and ang < 100:
+            p = 1
+        else:
+            p = 2
+        return p
+    def brazoPuntuation(self, ang):
+        # p para tener un codigo más limpio
+        if (ang < 20):
+            p = 1
+        elif (ang < 45):
+            p = 2
+        elif (ang < 90):
+            p = 3
+        else:
+            p = 4
+        return p
+
+    def cuelloPuntuation(self, ang):
+        # p para tener un codigo más limpio
+        if (ang <= 180 and ang>=170):
+            p = 1
+        elif (ang <= 170 and ang>=160):
+            p = 2
+        elif (ang <= 180 and ang>=170):
+            p = 3
+        else:
+            p = 4
+        return p
+
+    def promDot(self,img, p1, p2,draw=True):
+        z1, x1, y1 = self.lmList[p1]
+        z2, x2, y2 = self.lmList[p2]
+
+        # print(x1," , ",y1, " , ",z1)
+        # Calculo de punto
+        vector = [(x1+x2)/2, (y1+y2)/2, (z1+z2)/2]
+        if draw:
+            cv2.circle(img, (int(vector[0]), int(vector[1])), 7, (255, 0, 0), cv2.FILLED)
+        return vector
+
+
+    ## Funciones Johan
     def findVector(self, img, p1, p2, draw = True):
         x1, y1, z1 = self.lmList[p1]
         x2, y2, z2 = self.lmList[p2]
